990bb7643e2033c1354098c849f069914cb3b02d
ranandraj <ranandraj@gmail.com>
2020-10-03 12:53:34 +0000

    corrected typo resource<-s>

--- aks/logwrksp.tf
+++ aks/logwrksp.tf
@@ -1,4 +1,4 @@
-resources "azurerm_log_analytics_workspace" "alganwsp1" {
+resource "azurerm_log_analytics_workspace" "alganwsp1" {
     name = "lganwsp1"
     resource_group_name = "RG2"
     location = "westus2"
